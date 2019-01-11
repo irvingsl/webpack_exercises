6205de29b2e533d91aa5f2ff85ee6712088ae26f
irvingsl <irving.lima@vitavr.com>
2019-01-11 17:25:55 -0300

    Curso React, seção 3, até a aula 26

---
+++ ex.6/index.jsx
@@ -0,0 +1,7 @@
+import React from'react'
+import reactDOM from 'react-dom'
+
+ReactDOM.render(<h1>Olá React!</h1>, document.getElementById('app'))
+
+
+console.log('Funcionou!!!')
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const webpack = require('webpack')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+//const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-    entry: './ex/index.js',
+    entry: './ex/index.jsx',
     output:{
         path: __dirname + '/public',
         filename: './bundle.js'
@@ -11,21 +11,22 @@ module.exports = {
         port: 8080,
         contentBase: './public'
     },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    /*
     plugins: [
         new ExtractTextPlugin('app.css')
     ],
+    */
     module: {
         loaders: [{
-            test: /.js?$/,
+            test: /.jsx?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
                 presets: ['es2015', 'react'],
-                plugins: ['transform-object-rest-spread']
             }
-        },{
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract("style-loader", "css-loader")
         }]
     }
 }
